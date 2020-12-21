e77f48fcfc319d1ac12997daaeb457ab97cee968
Olli Knuuttila <olli.knuuttila@gmail.com>
2020-12-21 18:48:35 +0200

    fix prev

--- navex/losses/r2d2.py
+++ navex/losses/r2d2.py
@@ -11,11 +11,14 @@ from .peakiness import PeakinessLoss
 
 
 class R2D2Loss(BaseLoss):
-    def __init__(self, wdt=1.0, wap=1.0, det_n=16, base=0.5, nq=20, sampler=None):
+    def __init__(self, wc=1.0, wdt=1.0, wap=1.0, det_n=16, base=0.5, nq=20, sampler=None):
         super(R2D2Loss, self).__init__()
 
-        self.wdt = wdt if wdt >= 0 else nn.Parameter(torch.Tensor([-math.log(-wdt)]))
-        self.wap = wap if wap >= 0 else nn.Parameter(torch.Tensor([-math.log(-wap)]))
+        # self.wdt = wdt if wdt >= 0 else nn.Parameter(torch.Tensor([-math.log(-wdt)]))
+        # self.wap = wap if wap >= 0 else nn.Parameter(torch.Tensor([-math.log(-wap)]))
+        self.wc = wc if wc >= 0 else nn.Parameter(torch.Tensor([-wc]))
+        self.wdt = wdt if wdt >= 0 else nn.Parameter(torch.Tensor([-wdt]))
+        self.wap = wap if wap >= 0 else nn.Parameter(torch.Tensor([-wap]))
 
         self.ap_loss = AveragePrecisionLoss(base=base, nq=nq, sampler_conf=sampler)
         self.cosim_loss = CosSimilarityLoss(det_n)
@@ -40,14 +43,19 @@ class R2D2Loss(BaseLoss):
         a_loss = self.ap_loss(des1, des2, qlt1, qlt2, sc_aflow)
 
         # maybe optimize weights during training
-        p_loss = (self.wdt * p_loss) if isinstance(self.wdt, float) else (torch.exp(-self.wdt) * p_loss + self.wdt)
-        c_loss = (self.wdt * c_loss) if isinstance(self.wdt, float) else (torch.exp(-self.wdt) * c_loss + self.wdt)
-        a_loss = (self.wap * a_loss) if isinstance(self.wap, float) else (torch.exp(-self.wap) * a_loss + self.wap)
+        # p_loss = (self.wdt * p_loss) if isinstance(self.wdt, float) else (torch.exp(-self.wdt) * p_loss + self.wdt)
+        # c_loss = (self.wdt * c_loss) if isinstance(self.wdt, float) else (torch.exp(-self.wdt) * c_loss + self.wdt)
+        # a_loss = (self.wap * a_loss) if isinstance(self.wap, float) else (torch.exp(-self.wap) * a_loss + self.wap)
+        p_loss = self.wdt * torch.log(p_loss) + (0 if isinstance(self.wdt, float) else -torch.log(self.wdt))
+        c_loss = self.wc * torch.log(c_loss) + (0 if isinstance(self.wc, float) else -torch.log(self.wc))
+        a_loss = self.wap * torch.log(a_loss) + (0 if isinstance(self.wap, float) else -torch.log(self.wap))
 
         return p_loss + c_loss + a_loss
 
     def params_to_optimize(self, split=False):
         params = []
+        if not isinstance(self.wc, float):
+            params.append(self.wc)
         if not isinstance(self.wdt, float):
             params.append(self.wdt)
         if not isinstance(self.wap, float):
