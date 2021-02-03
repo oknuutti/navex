1551098da1f7fa1239d1fd50c0e0b458ce9a6d96
Olli Knuuttila <olli.knuuttila@gmail.com>
2021-02-03 15:15:06 +0200

    check WebImageSynthPairDataset for bad images

--- navex/datasets/revisitop1m.py
+++ navex/datasets/revisitop1m.py
@@ -10,6 +10,8 @@ class WebImageSynthPairDataset(SynthesizedPairDataset, AugmentedDatasetMixin):
                  eval=False, rgb=False, npy=False):
         assert not npy, '.npy format not supported'
         self.npy = npy
+        self.min_size = int(image_size*0.75)
+        self.max_aspect_ratio = 2.5
 
         AugmentedDatasetMixin.__init__(self, noise_max=noise_max, rnd_gain=rnd_gain, image_size=image_size,
                                        max_sc=max_sc, eval=eval, rgb=rgb, blind_crop=True)
@@ -18,16 +20,27 @@ class WebImageSynthPairDataset(SynthesizedPairDataset, AugmentedDatasetMixin):
                                         max_shear=max_shear, max_proj=max_proj, min_size=image_size//2,
                                         transforms=self.transforms)
 
-    def _recurse(self, path, samples):
+    def _recurse(self, path, samples, test):
         for fname in os.listdir(path):
             fullpath = os.path.join(path, fname)
             if fname[-4:] == ('.npy' if self.npy else '.jpg'):
-                samples.append(fullpath)
+                ok = not test
+                if test:
+                    try:
+                        img = self.image_loader(fullpath)
+                        ok = (img.size[0] >= self.min_size and img.size[1] >= self.min_size
+                              and max(img.size)/min(img.size) <= self.max_aspect_ratio)
+                    except Exception as e:
+                        print('%s' % e)
+                if ok:
+                    samples.append(fullpath)
+                else:
+                    print('rejected: %s' % fullpath)
             elif os.path.isdir(fullpath):
-                self._recurse(fullpath, samples)
+                self._recurse(fullpath, samples, test)
 
-    def _load_samples(self):
+    def _load_samples(self, test=True):
         samples = []
-        self._recurse(self.root, samples)
+        self._recurse(self.root, samples, test)
         samples = sorted(samples)
         return samples
