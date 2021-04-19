137ff1abbf1a2de1ecca60b884d9c35d53699b44
Olli Knuuttila <olli.knuuttila@gmail.com>
2021-04-19 18:15:47 +0300

    fix prev

--- navex/trials/base.py
+++ navex/trials/base.py
@@ -60,7 +60,7 @@ class TrialBase(abc.ABC, torch.nn.Module):
             except:
                 ok = False
         elif p[0] == 'optimizer':
-            pm = {'learning_rate': 'lr', 'weight_decay': 'weight_decay', 'eps': 'eps'}
+            pm = {'learning_rate': 'lr', 'lr': 'lr', 'weight_decay': 'weight_decay', 'eps': 'eps'}
             if len(p) > 1 and p[1] in pm:
                 for pg in self.optimizer.param_groups:
                     setattr(pg, pm[p[1]], value)
