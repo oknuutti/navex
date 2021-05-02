476277f28de10009c04a32b89ebde392088ced96
Olli Knuuttila <olli.knuuttila@gmail.com>
2021-05-03 01:40:41 +0300

    fix prev

--- navex/losses/ap.py
+++ navex/losses/ap.py
@@ -21,11 +21,11 @@ class DiscountedAPLoss(Module):
 
         # TODO: update config
         c = sampler_conf
-        self.sampler = DetectionSampler(pos_r=c['pos_d'], neg_min_r=c['neg_d'], neg_max_r=c['ngh'],
+        self.sampler = DetectionSampler(pos_r=c['pos_d'], neg_min_r=c['neg_d'], neg_max_r=c['neg_d'] + c['ngh'],
                                         neg_step=c['subd'], cell_d=abs(c['subq']), border=c['border'],
                                         max_neg_b=c['max_neg_b'], random=sampler_conf['subd_neg'] != 0)
 
-        self.calc_ap = DifferentiableAP(bins=nq, euclidean=True)
+        self.calc_ap = DifferentiableAP(bins=nq, euclidean=False)  # eucl perf worse, maybe due to lower mid ap res
         self.bce_loss = BCELoss(reduction='none')
 
     def forward(self, output1, output2, aflow):
