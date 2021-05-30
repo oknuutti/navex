d8bc64b4a82f9944aee94dfc7f76b4126edc0319
Olli Knuuttila <olli.knuuttila@gmail.com>
2021-05-30 16:17:02 +0300

    fix prev

--- navex/losses/peakiness.py
+++ navex/losses/peakiness.py
@@ -33,9 +33,11 @@ class ActivationLoss(Module):
     def __init__(self, sparsity, n):
         super(ActivationLoss, self).__init__()
         self.sparsity = sparsity
+        self.avg_pool_3 = AvgPool2d(3, stride=1, padding=1)
         self.avg_pool_n = AvgPool2d(n + 1, stride=1, padding=n // 2)
 
     def forward(self, det1, det2):
+        det1, det2 = map(self.avg_pool_3, (det1, det2))
         mean_activation1, mean_activation2 = map(self.avg_pool_n, (det1 + self.sparsity, det2 + self.sparsity))
         loss = (mean_activation1.pow(2).mean() + mean_activation2.pow(2).mean()) / 2
         return loss
