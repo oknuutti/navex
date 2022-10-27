4fb5acd69fa7316422d6076f89ff39f8082951da
Olli Knuuttila <olli.knuuttila@gmail.com>
2022-10-27 19:34:31 +0300

    explicit nan exclusion

--- navex/losses/cosim.py
+++ navex/losses/cosim.py
@@ -4,24 +4,11 @@ from torch.functional import F
 
 
 class CosSimilarityLoss(Module):
-    def __init__(self, n=16, use_max=False, implicit_nan_exclusion=False):
+    def __init__(self, n=16, use_max=False, exclude_nans=True):
         super(CosSimilarityLoss, self).__init__()
         self.patches = Unfold(n, padding=0, stride=n // 2)
         self.use_max = use_max
-        self.implicit_nan_exclusion = implicit_nan_exclusion
-
-    def extract_patches(self, det):
-        patches = self.patches(det).transpose(1, 2)
-        if self.use_max:
-            pass
-        elif self.implicit_nan_exclusion:
-            patches = F.normalize(patches, p=2, dim=2)
-        else:
-            patches[patches.isnan()] = 0
-            norm2 = patches.pow(2).sum(dim=2, keepdim=True)
-            norm2[norm2 == 0] = 1
-            patches = patches / torch.sqrt(norm2)
-        return patches
+        self.exclude_nans = exclude_nans    # For original R2D2 set exclude_nans = False
 
     def forward(self, det1, det2, aflow):
         H, W = aflow.shape[2:]
@@ -34,26 +21,25 @@ class CosSimilarityLoss(Module):
         grid[grid.isnan()] = 1e6    # still, on 2022-10-26, torch grid_sample does not handle nans correctly
                                     # (https://github.com/pytorch/pytorch/issues/24823)
 
-        warped_det2 = F.grid_sample(det2, grid, mode='bilinear', padding_mode='zeros', align_corners=False)
+        warped_det2 = F.grid_sample(det2, grid, mode='bilinear', padding_mode='border', align_corners=False)
 
-        if not self.implicit_nan_exclusion and not self.use_max:
+        if self.exclude_nans and not self.use_max:
             warped_det2[aflow[:, 0:1, :, :].isnan()] = float('nan')
 
         # patches are already normalized to unit length (possibly taking into account nans)
-        patches1 = self.extract_patches(det1)
-        patches2 = self.extract_patches(warped_det2)
+        patches1 = self.patches(det1).transpose(1, 2)
+        patches2 = self.patches(warped_det2).transpose(1, 2)
+
+        if self.exclude_nans and not self.use_max:
+            nans = patches2.isnan()
+            patches1[nans] = 0
+            patches2[nans] = 0
 
         if self.use_max:
             cosim = (patches1 * patches2).amax(dim=2)
-            loss = 1 - torch.mean(cosim)
-        elif self.implicit_nan_exclusion:
-            cosim = (patches1 * patches2).sum(dim=2)
-            loss = 1 - torch.mean(cosim)
         else:
-            mask = patches2.isnan()
-            patches2[mask] = 0
+            patches1 = F.normalize(patches1, p=2, dim=2)
+            patches2 = F.normalize(patches2, p=2, dim=2)
             cosim = (patches1 * patches2).sum(dim=2)
-            valids = mask.logical_not().sum(dim=2) > 0
-            loss = 1 - cosim.sum() / max(1, valids.sum())  # ignore patches with all nans
 
-        return loss
+        return 1 - torch.mean(cosim)
